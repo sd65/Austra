0758e039ed07069a42bbb362c27940d1a1c83218
SoEnguy <enguerran.henner@gmail.com>
2013-11-14 10:53:37 +0100

    Test hotfix

--- include/edt_display.php
+++ include/edt_display.php
@@ -8,6 +8,7 @@ function displayBy($hour, $tp, $td, $day_name, $week) {
 	$day;
 	$class_name;
 	$class_teacher;
+	$SRC_S3 = "SRC_S3";
 	
 	$hour = ($hour*60)+8;
 	
@@ -15,7 +16,7 @@ function displayBy($hour, $tp, $td, $day_name, $week) {
 		$day = day_to_int($day_name);
 	endfor;
 	
-	$sql = 'SELECT matiereedt,enseignantedt FROM edt WHERE jouredt=' . $day . ' AND semaineedt=' . $week . ' AND debutedt>=' . $hour . ' AND finedt<=' . $hour . ' AND groupeedt=SRC_S3';
+	$sql = 'SELECT matiereedt,enseignantedt FROM edt WHERE jouredt=' . $day . ' AND semaineedt=' . $week . ' AND debutedt=' . $hour . ' AND groupeedt=' . $SRC_S3 . '';
 	
 	$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 	
