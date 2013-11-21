b6cd0321d055854ada44c8a39a4505fd52f38a00
sd <sd@x53.(none)>
2013-11-21 18:09:07 +0100

    Added the intergration with HTML, CSS and more

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-	<meta charset="utf-8">
-	<title>Austra</title>
-	<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-	
 <?php
 	include "include/edt_display.php";
 ?>
@@ -17,8 +8,19 @@
 	$td = 1; //Donnée renseignée pour test
 ?>
 
-	<div id="page">
-		<h1>Austra</h1>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Austra</title>
+        <meta name="description" content="" />
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="./css/base.css" />
+        <link rel="stylesheet" type="text/css" href="./css/table.css" />
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.0.min.js"></script>
+        <script type="text/javascript" src="./js/frontend.js"></script>
+    </head>
+    <body>
 		<div id="table">
 			<table>        
 				<thead>
@@ -35,11 +37,168 @@
 			</tbody>
 			</table>
 		</div> <!-- Fin Tableau -->
+        <aside id="menu">
+            <ul>
+                <p>Affichage</p>
+                <li class="switch">
+                    Tous
+                    <div class="onoffswitch">
+                        <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch">
+                        <label class="onoffswitch-label" for="myonoffswitch">
+                            <div class="onoffswitch-inner"></div>
+                            <div class="onoffswitch-switch"></div>
+                        </label>
+                    </div>
+                    Moi
+                </li>
+                <li><a href="">TP1</a><a href="" class="current">TP2</a><a href="">TP3</a></li>
+                <li><a href="">TD1</a><a href="" class="current">TD2</a></li>
 
-		<!-- <aside class="calendar">
-			<h1>Welcome</h1>
-		</aside> -->
-		
-	</div> <!-- Fin Page -->
-</body>
+                <p>Paramètres</p>
+                <li><a href="">Modifier mon profil</a></li>
+
+                <li class="logout"><a href="">Déconnexion</a></li>
+            </ul>
+        </aside>
+        <nav>
+            <a href="javascript:;" class="user">Ludovic Boussion</a>
+            <a href="javascript:;" class="closecalendars">Calendrier</a>
+        </nav>
+         
+        <aside id="calendars">
+            <div class="month">
+               <div>Octobre</div>
+                    <ul>
+                       <li class="notinmonth">30</li><li>1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li>
+                   </ul>
+                    <ul>
+                       <li>7</li><li>8</li><li>9</li><li>10</li><li>11</li><li>12</li><li>13</li>
+                   </ul>
+                    <ul class="active">
+                       <li>14</li><li>15</li><li>16</li><li>17</li><li>18</li><li>19</li><li>20</li>
+                   </ul>
+                    <ul>
+                       <li>21</li><li>22</li><li>23</li><li>24</li><li>25</li><li>26</li><li>27</li>
+                   </ul>
+                    <ul>
+                       <li>28</li><li>29</li><li>30</li><li>31</li><li class="notinmonth">1</li><li class="notinmonth">2</li><li class="notinmonth">3</li>
+                   </ul>
+            </div>
+
+            <div class="month">
+               <div>Novembre</div>
+                    <ul>
+                       <li class="notinmonth">28</li><li class="notinmonth">29</li><li class="notinmonth">30</li><li class="notinmonth">31</li><li>1</li><li>2</li><li>3</li>
+                   </ul>
+                    <ul>
+                       <li>4</li><li>5</li><li>6</li><li>7</li><li>8</li><li>9</li><li>10</li>
+                   </ul>
+                    <ul>
+                       <li>11</li><li>12</li><li>13</li><li>14</li><li>15</li><li>16</li><li>17</li>
+                   </ul>
+                    <ul>
+                       <li>18</li><li>19</li><li>20</li><li>21</li><li>22</li><li>23</li><li>24</li>
+                   </ul>
+                    <ul>
+                       <li>25</li><li>26</li><li>27</li><li>28</li><li>29</li><li>30</li><li class="notinmonth">1</li>
+                   </ul>
+            </div>
+
+            <div class="month">
+               <div>Décembre</div>
+                    <ul>
+                       <li class="notinmonth">25</li><li class="notinmonth">26</li><li class="notinmonth">27</li><li class="notinmonth">28</li><li class="notinmonth">29</li><li class="notinmonth">30</li><li>1</li>
+                   </ul>
+                    <ul>
+                       <li>2</li><li>3</li><li>4</li><li>5</li><li>6</li><li>7</li><li>8</li>
+                   </ul>
+                    <ul>
+                       <li>9</li><li>10</li><li>11</li><li>12</li><li>13</li><li>14</li><li>15</li>
+                   </ul>
+                    <ul>
+                       <li>16</li><li>17</li><li>18</li><li>19</li><li>20</li><li>21</li><li>22</li>
+                   </ul>
+                    <ul>
+                       <li>23</li><li>24</li><li>25</li><li>26</li><li>27</li><li>28</li><li>29</li>
+                   </ul>
+                   <ul>
+                       <li>30</li><li>31</li><li class="notinmonth">1</li><li class="notinmonth">2</li><li class="notinmonth">3</li><li class="notinmonth">4</li><li class="notinmonth">5</li>
+                   </ul>
+            </div>
+
+            <div class="month">
+               <div>Janvier</div>
+                    <ul>
+                       <li class="notinmonth">30</li><li class="notinmonth">31</li><li>1</li><li>2</li><li>3</li><li>4</li><li>5</li>
+                   </ul>
+                    <ul>
+                       <li>6</li><li>7</li><li>8</li><li>9</li><li>10</li><li>11</li><li>12</li>
+                   </ul>
+                    <ul>
+                       <li>13</li><li>14</li><li>15</li><li>16</li><li>17</li><li>18</li><li>19</li>
+                   </ul>
+                    <ul>
+                       <li>20</li><li>21</li><li>22</li><li>23</li><li>24</li><li>25</li><li>26</li>
+                   </ul>
+                    <ul>
+                       <li>27</li><li>28</li><li>29</li><li>30</li><li>31</li><li class="notinmonth">1</li><li class="notinmonth">2</li>
+                   </ul>
+            </div>
+
+            <div class="month">
+               <div>Novembre</div>
+                    <ul>
+                       <li class="notinmonth">28</li><li class="notinmonth">29</li><li class="notinmonth">30</li><li class="notinmonth">31</li><li>1</li><li>2</li><li>3</li>
+                   </ul>
+                    <ul>
+                       <li>4</li><li>5</li><li>6</li><li>7</li><li>8</li><li>9</li><li>10</li>
+                   </ul>
+                    <ul>
+                       <li>11</li><li>12</li><li>13</li><li>14</li><li>15</li><li>16</li><li>17</li>
+                   </ul>
+                    <ul>
+                       <li>18</li><li>19</li><li>20</li><li>21</li><li>22</li><li>23</li><li>24</li>
+                   </ul>
+                    <ul>
+                       <li>25</li><li>26</li><li>27</li><li>28</li><li>29</li><li>30</li><li class="notinmonth">1</li>
+                   </ul>
+            </div>
+
+            <div class="month">
+               <div>Décembre</div>
+                    <ul>
+                       <li class="notinmonth">25</li><li class="notinmonth">26</li><li class="notinmonth">27</li><li class="notinmonth">28</li><li class="notinmonth">29</li><li class="notinmonth">30</li><li>1</li>
+                   </ul>
+                    <ul>
+                       <li>2</li><li>3</li><li>4</li><li>5</li><li>6</li><li>7</li><li>8</li>
+                   </ul>
+                    <ul>
+                       <li>9</li><li>10</li><li>11</li><li>12</li><li>13</li><li>14</li><li>15</li>
+                   </ul>
+                    <ul>
+                       <li>16</li><li>17</li><li>18</li><li>19</li><li>20</li><li>21</li><li>22</li>
+                   </ul>
+                    <ul>
+                       <li>23</li><li>24</li><li>25</li><li>26</li><li>27</li><li>28</li><li>29</li>
+                   </ul>
+                   <ul>
+                       <li>30</li><li>31</li><li class="notinmonth">1</li><li class="notinmonth">2</li><li class="notinmonth">3</li><li class="notinmonth">4</li><li class="notinmonth">5</li>
+                   </ul>
+            </div>
+            <div class="numWeek">
+            <ul>
+              <li>40</li><li>41</li><li>42</li><li>43</li><li>44</li>
+            </ul>
+            <ul>
+              <li>44</li><li>45</li><li>46</li><li>47</li><li>48</li>
+            </ul>
+            <ul>
+              <li>48</li><li>49</li><li>50</li><li>51</li><li>52</li><li>1</li>
+            </ul>
+            <ul>
+              <li>1</li><li>2</li><li>3</li><li>4</li><li>5</li>
+            </ul>
+            </div>
+        </aside>
+    </body>
 </html>
---
+++ js/frontend.js
@@ -0,0 +1,48 @@
+$(document).ready(function() 
+{   
+	var isMenuOpen = false;
+	$('.user').click(function()
+    {
+		if (isMenuOpen == false){
+            $("#menu").clearQueue().animate({
+				left : '0'
+            })
+            $(".user").clearQueue().animate({
+                "margin-left" : '220px'
+            })
+			isMenuOpen = true;
+		}else{
+            $("#menu").clearQueue().animate({
+                left : '-200px'
+            })
+            $(".user").clearQueue().animate({
+                "margin-left" : '0px'
+            })
+			isMenuOpen = false;
+		}
+    });
+
+	var isCalOpen = false;
+	$('.closecalendars').click(function()
+    {
+		if (isCalOpen == false){
+            $("#calendars").fadeIn().clearQueue().animate({
+				right : '0px'
+            })
+			$(".closecalendars").clearQueue().animate({
+                "margin-right" : '345px',
+				"display" : 'block'
+            })
+			isCalOpen = true;
+		}else{
+            $("#calendars").fadeOut().clearQueue().animate({
+                right : '-300px'
+            })
+			$(".closecalendars").clearQueue().animate({
+                "margin-right" : '45px'
+            })
+			isCalOpen = false;
+		}
+    });
+
+});
