49394763fe5854b53c2f2c0b7ca85c65e6d7ad5b
sd65 <sylvain.doignon@gmail.com>
2013-12-12 14:02:04 +0100

    Deletedement :)

--- admin/include/aside.php
+++
@@ -1,8 +0,0 @@
-<aside>
-	<ul>
-		<li><a class="pageactive" href="<?php  ?>">Cours</a></li>  
-		<li><a href="<?php  ?>">Élèves</a></li>  
-		<li><a href="<?php  ?>">Enseignants</a></li>  
-		<li><a href="<?php  ?>">Salles</a></li>  
-	</ul>
-</aside>
--- admin/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-	include("./include/header.php"); 
-	include("./include/aside.php"); 
-	include("./include/footer.php");
-?>
