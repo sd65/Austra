f09e477b04daa66bfc5b1ee8b223ad3924d657e4
Marie Aline Millot <mariealinemillot@gmail.com>
2013-12-12 13:04:08 +0100

    Add admin skeleton with include and css

---
+++ admin/include/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Austra - Administration</title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<link rel="shortcut icon" href="./img/favicon.png">
+	<link rel="stylesheet" type="text/css" href="./css/base.css">
+	<link rel="stylesheet" type="text/css" href="./css/admin.css">
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.0.min.js"></script>
+</head>
+<body>
+	<header>
+		<a href="<?php  ?>"><img src="./img/logo@2x.png" alt="Austra" width="140" height"48"/></a>
+		<ul>
+			<li><a href="">MMI 1A</a></li>
+			<li><a class="pageactive" href="">SRC 2A</a></li>
+			<li><a href="">PUB 1A</a></li>
+			<li><a href="">PUB 2A</a></li>
+			<li><a href="">LP</a></li>
+		</ul>
+		<input type="search" name="cours" placeholder="Rechercher un cours">
+		<a class="boutonright" href="">Ajouter</a> 
+	</header>
+	
---
+++ admin/index.php
@@ -0,0 +1,5 @@
+<?php
+	include("./include/header.php"); 
+	include("./include/aside.php"); 
+	include("./include/footer.php");
+?>
