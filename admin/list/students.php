ba66b5925b9eebde337b950a29a60600552b5574
Marie Aline Millot <mariealinemillot@gmail.com>
2014-01-09 16:33:20 +0100

    Modify email SQL query

--- admin/list/students.php
+++ admin/list/students.php
@@ -22,14 +22,22 @@ include "../include/header.php" ;
 
 	<tbody>
 		<?php 
-
-		$req = $bdd->prepare('SELECT DISTINCT prenom, nometudiant, email, promo, dept FROM etudiant WHERE filiere=:filiere');
-        $req->execute(array('filiere' => $_GET['filiere']));
-        ?>
+		if(isset($_GET['filiere'])){
+			$req = $bdd->prepare('SELECT DISTINCT prenom, nometudiant, email, promo, dept FROM etudiant WHERE filiere=:filiere');
+	        $req->execute(array('filiere' => $_GET['filiere']));
+    }
+    ?>
             <?php 
             while ($listeEtudiant = $req->fetch()): ?>
 				<tr>
-					<td class="name"><?=$listeEtudiant['nometudiant'];?></td><td class="surname"><?=$listeEtudiant['prenom'];?></td><td class="course"><?=$listeEtudiant['dept'];?></td><td class="year"><?=$listeEtudiant['promo'];?></td><td>TD1</td><td>TP1</td><td>Chinois</td><td class="mail"><?=$listeEtudiant['mail'];?></td>
+					<td class="name"><?=$listeEtudiant['nometudiant'];?></td>
+					<td class="surname"><?=$listeEtudiant['prenom'];?></td>
+					<td class="course"><?=$listeEtudiant['dept'];?></td>
+					<td class="year"><?=$listeEtudiant['promo'];?></td>
+					<td>TD1</td>
+					<td>TP1</td>
+					<td>Chinois</td>
+					<td class="mail"><?=$listeEtudiant['email'];?></td>
 					<td class="options"><a class="show" href=""><a class="edit" href=""></td>
 					<td class="options"><a class="show" href=""><a class="edit" href=""></td>
 					<td class="options"><a class="show" href=""></a><a class="edit" href=""></a><a class="delete" href=""></a></td>
