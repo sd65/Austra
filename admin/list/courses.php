a507fc67c4d45e26e77456de1614a5db3d7b581c
Marie Aline Millot <mariealinemillot@gmail.com>
2014-01-09 16:34:47 +0100

    Implement SQL queries in Courses view

--- admin/list/courses.php
+++ admin/list/courses.php
@@ -3,23 +3,37 @@ $metier = "cours";
 include_once "../../include/db_connect.php";
 include "../include/header.php" ;
 ?>
+<table>
+  <thead>
+    <td>Matière</td>
+    <td>Filière</td>
+    <td>UE</td>
+    <td>Module</td>
+    <td>Enseignant</td>
+    <!-- <td>Mémo</td> -->
+    <td>Options</td>
+  </thead>
+  <tbody>
+    <?php 
 
-        <table>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-          <tr><td class="intitule">Réseaux internet et services</td><td class="uemodu">UE1</td><td class="uemodu">M22</td><td class="memo">Mémo</td><td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td></tr>
-        </table>
-    </body>
+    $req = $bdd->prepare('SELECT DISTINCT nommatiere, dept, modulematiere, codematiere, enseignantmatiere FROM matiere');
+    $req->execute();
+    ?>
+    <?php 
+    while ($listeCours = $req->fetch()): ?>
+    <tr>
+      <td class="intitule"><?=$listeCours['nommatiere']?></td>
+      <td class="filiere"><?=$listeCours['dept']?></td>
+      <td class="uemodu"><?=$listeCours['modulematiere']?>
+      </td><td class="module"><?=$listeCours['codematiere']?></td>
+      </td><td class="enseignant"><?=$listeCours['enseignantmatiere']?></td>
+      <!-- <td class="memo">Mémo</td> -->
+      <td><a class="edit hover" href=""></a><a class="delete hover" href=""></a></td>
+    </tr>
+    <?php
+    endwhile;
+    ?>
+  </tbody>
+</table>
+</body>
 </html>
