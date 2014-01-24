fbed47a07e50aeabfd61e721626df04364ca4f4c
SoEnguy <enguerran.henner@gmail.com>
2014-01-24 17:16:41 +0100

    Changed img size, added password modification to teacher

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,3 @@
 <?php 
 include "./include/aside.php" ;
-
-session_start();
-
-if(empty($_SESSION['prenomenseignant']) || empty($_SESSION['nomenseignant'])  || empty($_SESSION['departementenseignant']) || (!$_SESSION['niveauacces'] < 50)) {header('Location: ../index.php');}
  ?>
