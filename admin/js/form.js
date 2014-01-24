0fd4f527bb2b9e3f18634a2eef89bd390f24056c
LudovicBou <ludovic.boussion@gmail.com>
2014-01-24 13:49:11 +0100

    Ajout de 2 colonnes : Heures statutaires et Heures plannifiées

--- admin/js/form.js
+++ admin/js/form.js
@@ -1,18 +1,10 @@
 $(document).ready(function(){
-	if($('#invernant').is(':checked')){
+	if(($('#invernant').is(':checked')) || ($('#autre').is(':checked'))){
 		$(".pro").clearQueue().css({
 				display : 'block'
         });
 	}
 	if($('#autre').is(':checked')){
-		$(".pro").clearQueue().css({
-				display : 'block'
-        });
-	}
-	if($('#autre').is(':checked')){
-		$(".pro").clearQueue().css({
-				display : 'block'
-        });
 		$("#autreinput").clearQueue().css({
 				display : 'block'
         });
@@ -20,8 +12,8 @@ $(document).ready(function(){
 		$("#autreinput").clearQueue().css({
 				display : 'none'
         });
-
 	}
+
 	$('#invernant').click(function() {
         $(".pro").clearQueue().css({
 				display : 'block'
@@ -30,56 +22,25 @@ $(document).ready(function(){
 				display : 'none'
         });
 		$("#autre").removeAttr('checked').checkboxradio('refresh');
-
 	});
 
 	$('#autre').click(function() {
         $(".pro").clearQueue().css({
 				display : 'block'
         });
-	});
-
-
-	$('#Enseignant').click(function() {
-        $(".pro").clearQueue().css({
-				display : 'none'
-        });
-		$("#autreinput").clearQueue().css({
-				display : 'none'
-        });
-			$("#autre").removeAttr('checked').checkboxradio('refresh');
-
-	});
-	$('#department_chief').click(function() {
-        $(".pro").clearQueue().css({
-				display : 'none'
-        });
-		$("#autreinput").clearQueue().css({
-				display : 'none'
-        });
-				$("#autre").removeAttr('checked').checkboxradio('refresh');
-
-	});
-	$('#personnel-administratif').click(function() {
-        $(".pro").clearQueue().css({
-				display : 'none'
-        });
-		$("#autreinput").clearQueue().css({
-				display : 'none'
-        });
-				$("#autre").removeAttr('checked').checkboxradio('refresh');
-
-
-	});
-
-
-	$('#autre').click(function() {
         $("#autreinput").clearQueue().css({
 				display : 'block'
         });
 		$("#personnel-administratif, #invernant, #department_chief, #Enseignant").removeAttr('checked').checkboxradio('refresh');
 	});
 
-
-
+	$('#department_chief, #Enseignant, #personnel-administratif').click(function() {
+        $(".pro").clearQueue().css({
+				display : 'none'
+        });
+		$("#autreinput").clearQueue().css({
+				display : 'none'
+        });
+		$("#autre").removeAttr('checked').checkboxradio('refresh');
+	});
 });
