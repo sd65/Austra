f92cd483c58a27b7d404f957b0058fa66fda3391
SoEnguy <enguerran.henner@gmail.com>
2014-01-24 16:22:06 +0100

    Added security for accessing admin panel

--- admin/include/aside.php
+++ admin/include/aside.php
@@ -1,5 +1,7 @@
 <!-- HEADER SUBJECT = TRI PAR FILIERE -->
+<?php session_start();
 
+if(empty($_SESSION['prenomenseignant']) || empty($_SESSION['nomenseignant'])  || empty($_SESSION['departementenseignant']) || (!$_SESSION['niveauacces'] < 50)) {header('Location: ../index.php');} ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,7 @@
 <?php 
 include "./include/aside.php" ;
 
+session_start();
+
+if(empty($_SESSION['prenomenseignant']) || empty($_SESSION['nomenseignant'])  || empty($_SESSION['departementenseignant']) || (!$_SESSION['niveauacces'] < 50)) {header('Location: ../index.php');}
  ?>
